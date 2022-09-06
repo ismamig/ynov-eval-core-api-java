ceadc94d0a30083358fca52a2bca2671b41bc3ca
Clément HERVE <clement-herve@asys.fr>
2022-09-06 17:49:57 +0200

    fix: compliance to requests

--- app/src/main/java/fr/asys/starter/cleher/core/api/config/openapi/OpenApiConfig.java
+++ app/src/main/java/fr/asys/starter/cleher/core/api/config/openapi/OpenApiConfig.java
@@ -1,17 +1,19 @@
 package fr.asys.starter.cleher.core.api.config.openapi;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().components(new Components()).info(new Info().title("Starter Clément HERVE - taskmanager api")
-                .description("A list of the available endpoints to manage your tasks"));
+        return new OpenAPI().components(new Components())
+                .info(new Info().title("Starter Clément HERVE - taskmanager api")
+                        .description("A list of the available endpoints to manage your tasks"));
     }
 }
